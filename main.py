6cd98a1fc894ab50a844a2b591f308539a9bb1c0
Jackson Zenti <130002884+jentille@users.noreply.github.com>
2024-05-21 22:33:29 -0700

    Adapted output_csr_as_txt() among other things
    I toyed around with adding tensors with pytorch but I figured that would be jumping the gun a bit on ALS and I wanted to check in with you first. Let me know what you think!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import csv
 import sys
 import pandas as pd
 from scipy import sparse
+from fastai.collab import *
+from fastai.tabular.all import *
 
 
 
@@ -44,29 +46,51 @@ def to_csr(dataframe:pd.DataFrame, user_name:str, item_name:str, weight_name:str
     coo = sparse.coo_matrix((dataframe[weight_name].astype(float), (dataframe[user_name], dataframe[item_name])), shape=(users+1,items+1))
     return coo.tocsr()
 
-def output_csr_as_txt(filename: str, csr: sparse._csr.csr_matrix):
+def output_csr_as_txt(filename: str, csr: sparse._csr.csr_matrix, column_names: list):
+    "Converts CSR input to txt file"
     file = open(filename,'w')
+    # Write the header with column names and types
+    header = ' '.join(column_names)
+    file.write(header + '\n')
+    # Write data
     for i in range(csr.shape[0]):
         for j in csr[i].nonzero()[1]:
-            file.write(str(i)+' ' +str(j)+' '+str(csr[i,j])+'\n')
+            file.write(f"{i} {j} {csr[i, j]}\n")
     file.close
-    return
-
-
+    
 
 if __name__=="__main__":
     #Args: python3 main.py <sparse-matrix> <index-class>
     #Maybe add userId, movieId, rating as arguments?
     if len(sys.argv) != 3:
-        print("Args need to be: python3 main.py <sparse-matrix> <index-class>")
+        print("Usage: python3 main.py <sparse-matrix> <index-class>")
         sys.exit(-1)
     #Load datasets into dataframes
     print("\tLoading movie/user matrix:", sys.argv[1], "\n\tUsing", sys.argv[2], "as key matrix.")
     train_user_items,test_user_items = load_user_item(sys.argv[1], "userId", "movieId", "rating")
     index_item_dict = load_index_item(sys.argv[2], "movieId", "title")
+    print(train_user_items.head())
+    print(index_item_dict.head())
+    merged = train_user_items.merge(index_item_dict, left_on="movieId", right_index=True)
+    print(merged.head())
+
+    # began messing with pytorch
+    # check this link https://www.kaggle.com/code/jhoward/collaborative-filtering-deep-dive
+    # dls = CollabDataLoaders.from_df(merged, item_name='title', bs=64)
+    # dls.show_batch()
+    # n_users  = len(dls.classes['user'])
+    # n_movies = len(dls.classes['title'])
+    # n_factors = 5
+
+    # user_factors = torch.randn(n_users, n_factors)
+    # movie_factors = torch.randn(n_movies, n_factors)
+    # Save the merged DataFrame to a new CSV file
+    # merged.to_csv('merged_output.csv', index=False)
+    
     #Load user-item dataframe into csr_matrix format
     #TODO: Get rid of extra row in csr_matrix? Or ignore?
     train_user_items = to_csr(train_user_items, "userId", "movieId", "rating")
-    output_csr_as_txt("user-movie-rating.txt", train_user_items)
+
+    output_csr_as_txt("user-movie-rating.txt", train_user_items, ["userId", "movieId", "rating"])
     print("successful!")
 
