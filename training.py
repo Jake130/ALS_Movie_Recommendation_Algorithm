a1e8fbe0d484c25222539c7b63b5d2f0eb92f2ce
Jackson Zenti <130002884+jentille@users.noreply.github.com>
2024-06-03 17:20:09 -0700

    Implemented MSE visualization as well as various hyperparameters you can tune
    I added a bunch of hyperparameters you can tune here I will give you a quick call about them. But please let me know if anything needs to be clarified.

--- training.py
+++ training.py
@@ -1,33 +1,47 @@
 import pandas as pd
 import random
 from math import log, exp, sqrt
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
 
 
 class ALS_Model():
-    def __init__(self, num_users, num_items, n_latency_factors, eta, max_iters, convergence_threshold, l2_factor):
-        #self.user_item_matrix = user_item_matrix
-        #self.user_matrix = user_matrix
-        #self.item_matrix = item_matrix
-        self.user_factors = self.randomize_narray(num_users, n_latency_factors)                  #Randomly Initialize
-        self.user_biases = self.randomize_narray(2, num_users)                   #Randomly Initialize
-        self.item_factors = self.randomize_narray(n_latency_factors, num_items)                 #Randomly Initialize
-        self.item_biases = self.randomize_narray(2, num_items)                  #Randomly initialize
+    def __init__(self, num_users, num_items, n_latency_factors, eta, max_iters, convergence_threshold, l2_factor, factor_scale, bias_scale):
+        """
+        Usage:
+
+        model = ALS_Model(<number of users>, <number of items>, <number of latent factors>, <learning rate>, <maximum iterations>, <convergence threshold>, <l2 lambda value>, <scalar for factor initialization>, <scalar for bias initialization>)
+        """
+        
+        # random.seed(42) # you can seed specific randomization to check how specific initialization will impact loss!
+        # Try changing the seed to any number and take note of the initial values and how they impact MSE calculation
+        self.factor_scale = factor_scale # an adjustable scalar for factor initialization
+        self.bias_scale = bias_scale # an adjustable scalar for bias initialization
+
+        self.num_users = num_users
+        self.num_items = num_items
+
+        self.user_factors = self.randomize_narray(num_users, n_latency_factors, factor_scale)                  #Randomly Initialize
+        self.user_biases = self.randomize_narray(1, num_users, bias_scale)                   #Randomly Initialize
+        self.item_factors = self.randomize_narray(n_latency_factors, num_items, factor_scale)                 #Randomly Initialize
+        self.item_biases = self.randomize_narray(1, num_items, bias_scale)                  #Randomly initialize
         self.n_latency_factors = n_latency_factors
         self.eta = eta
         self.max_iters = max_iters
         self.convergence_threshold = convergence_threshold
         self.l2_factor = l2_factor
 
-    def randomize_narray(self, M:int, N:int):
+    def randomize_narray(self, M:int, N:int, scale):
         empty = []
         if M<=1:
             for el in range(N):
-                empty.append(random.gauss(0.0, 1.0))
+                empty.append(random.gauss(0.0, scale))
         else:
             for row in range(M):
                 new = []
                 for el in range(N):
-                    new.append(random.gauss(0.0, 1.0))
+                    new.append(random.gauss(0.0, scale))
                 empty.append(new)
         return empty
 
@@ -61,10 +75,8 @@ class ALS_Model():
         
 
     def compute_gradients(self, training_data, ratings):
-        num_users = len(self.user_factors)
-        print(num_users)
-        num_items = len(self.item_factors[0])
-        print(num_items)
+        num_users = self.num_users
+        num_items = self.num_items
 
         # Initialize gradients with for loops
         grad_user_factors = []
@@ -90,11 +102,8 @@ class ALS_Model():
             # this formula is based on slide 6
             # Also did some more research and found this formula
             
-            
             # May want to add bias before subtracting actual rating
-            print("user_id: ", user_id)
             user_id = int(user_id)
-            print("item_id: ", item_id)
             item_id = int(item_id)
 
             # Compute y * (w^T x + b)
@@ -127,9 +136,11 @@ class ALS_Model():
         actual_score = self.user_item_matrix[user_id][item_id]
         pass
 
-    def train(self, training_data, ratings):
+    def train(self, training_data, ratings, test_data):
         """Implements ALS, switching between finetuning the user_factors
         and the movie_factors. Stop when both reach MAX_ITER or are converged?"""
+        train_losses = []
+        test_losses = []
         for iteration in range(self.max_iters):
             # Compute gradients
             grad_user_factors, grad_item_factors, grad_user_biases, grad_item_biases = self.compute_gradients(training_data, ratings)
@@ -138,12 +149,19 @@ class ALS_Model():
             for i in range(len(self.user_factors)):
                 for k in range(self.n_latency_factors):
                     self.user_factors[i][k] -= self.eta * grad_user_factors[i][k]
-                self.user_biases[0][i] -= self.eta * grad_user_biases[i]
+                self.user_biases[i] -= self.eta * grad_user_biases[i]
 
             for j in range(len(self.item_factors[0])):
                 for k in range(self.n_latency_factors):
                     self.item_factors[k][j] -= self.eta * grad_item_factors[j][k]
-                self.item_biases[0][j] -= self.eta * grad_item_biases[j]
+                self.item_biases[j] -= self.eta * grad_item_biases[j]
+
+            train_loss = self.means_squared_error(training_data, ratings)
+            test_loss = self.means_squared_error(test_data, ratings)
+            train_losses.append(train_loss)
+            test_losses.append(test_loss)
+
+            print(f"Iteration {iteration + 1}: Train MSE = {train_loss}, Test MSE = {test_loss}")
 
             # Compute gradient magnitude for convergence check
             gradient_magnitude = sqrt(
@@ -157,28 +175,15 @@ class ALS_Model():
                 print(f"Converged at iteration {iteration}")
                 break
 
-        return self.user_factors, self.item_factors, self.user_biases, self.item_biases
+        return self.user_factors, self.item_factors, self.user_biases, self.item_biases, train_losses, test_losses
     
     def predict_als(self, user_id, item_id):
         """Predict the probability of the correct rating label given the attributes, user_factor[i] & item_factor[i]"""
         return self.dot_product(user_id, item_id)
-    
-    def compute_accuracy(self, test_data, ratings):
-        """Compute accuracy of predictions against test data"""
-        total_error = 0.0
-        for (user_id, item_id), _ in test_data.iterrows():
-            user_id = int(user_id)
-            item_id = int(item_id)
-            predicted_rating = self.predict_als(user_id, item_id)
-            actual_rating = ratings[(user_id, item_id)]
-            total_error += (predicted_rating - actual_rating) ** 2
-        mse = total_error / len(test_data)
-        print("Mean Squared Error: ", mse)
-        return mse
 
 if __name__=="__main__":
     #This is for testing
-    als = ALS_Model(3, 4, 5, 0.01, 20, 0.001, 0.1)
+    als = ALS_Model(3, 4, 5, 0.001, 50, 0.0001, 0.1, 0.1, 1.0)
     print(als.user_factors)
     print("user biases: ", als.user_biases)
     print(als.item_factors)
@@ -193,19 +198,29 @@ if __name__=="__main__":
     print(als.dot_product(3, 4))
     print(als.sigmoid_range(als.dot_product(3,4)))
 
-    # Sample training data and ratings for testing
-    training_data = pd.DataFrame(index=[(0, 0), (0, 1), (1, 0), (1, 1), (2, 2)])
-    ratings = {(0, 0): 4, (0, 1): 3, (1, 0): 4, (1, 1): 5, (2, 2): 3}
+    # Testing with fake simulated data
+    all_data = [(i, j) for i in range(als.num_users) for j in range(als.num_items)]
+    # Set possible ratings
+    possible_ratings = [i / 2.0 for i in range(2, 11)]  # [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
 
-    # Train the model
-    als.train(training_data, ratings)
+    # Generate ratings for approximately 40% of the user-item pairs out of the possible ratings we just created
+    # We do this because we want to keep our data sparse, similar to real data, users will not have opinions about every item.
+    ratings = {(i, j): random.choice(possible_ratings) for i, j in all_data if random.random() > 0.6}
+    
+    # Convert to DataFrame
+    all_data_df = pd.DataFrame(index=ratings.keys())
 
-    # Compute accuracy
-    test_data = pd.DataFrame(index=[(0, 0), (1, 1), (2, 2)])
-    als.compute_accuracy(test_data, ratings)
+    # Split the data into training and test sets
+    # You can also add a random_state = parameter to this function to seed splits
+    training_data, test_data = train_test_split(all_data_df, test_size=0.3)
 
-    # grad_user_factors, grad_item_factors, grad_user_biases, grad_item_biases = als.compute_gradients(training_data, l2_factor, ratings)
-    # print(grad_user_factors)
-    # print(grad_item_factors)
-    # print(grad_user_biases)
-    # print(grad_item_biases)
+    # Train data
+    user_factors, item_factors, user_biases, item_biases, train_losses, test_losses = als.train(training_data, ratings, test_data)
+
+    plt.plot(train_losses, label='Training Loss')
+    plt.plot(test_losses, label='Testing Loss')
+    plt.xlabel('Iterations')
+    plt.ylabel('Mean Squared Error')
+    plt.legend()
+    plt.title('Training and Testing Loss over Iterations')
+    plt.show()
